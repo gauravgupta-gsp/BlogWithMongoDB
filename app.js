6635e70d31e169e9457ff4f1d832af2fe0cdbacc
gauravgupta-gsp <gauravgupta.gsp@gmail.com>
2020-05-21 09:59:43 +0530

    added port for heroku

--- app.js
+++ app.js
@@ -95,9 +95,13 @@ app.get("/posts/:postId", (req, res) => {
 });
 
 
+let port = process.env.PORT;
+if(port == null || port == "") {
+  port =3000;
+}
 
 
 
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
